64a01e4b35407c98e2c85814f9455e032f8697f0
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2024-03-10 17:13:47 +0300

    cleaned strpad functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tkasbari <thomas.kasbarian@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 18:40:07 by tkasbari          #+#    #+#             */
-/*   Updated: 2024/03/08 18:37:19 by tkasbari         ###   ########.fr       */
+/*   Updated: 2024/03/09 09:35:55 by tkasbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,7 +109,7 @@ size_t	ft_get_int_order_base(size_t nb, size_t base_len);
 char	*ft_itoa_base_signed(int n, char *base);
 char	*ft_itoa_base_unsigned(size_t n, char *base);
 char	*ft_strreplicate(char c, size_t len);
-char	*ft_strlpad(const char *s, char cpad, size_t target_size);
+char	*ft_strlpad(char *src, char padding, size_t target_size, bool free_src);
 char	*ft_strlpad_free(char *s, char cpad, size_t target_size, int to_free);
 char	*ft_strrpad(char *src, char padding, size_t target_size, bool free_src);
 char	*ft_strjoin_free(char *s1, char *s2, bool free_s1, bool free_s2);
